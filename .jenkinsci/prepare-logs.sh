adbf0e38a970456686c3fb93b448827b752d30e9
Sofiane Benahmed <38732323+ravenolf@users.noreply.github.com>
2020-06-18 15:26:27 +0300

    refactor (#22)
    Signed-off-by: Sofiane Benahmed <benahmed@soramitsu.co.jp>

---
+++ .jenkinsci/prepare-logs.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+mkdir -p build-logs
+
+while read -r LINE 
+do
+  docker logs $(echo $LINE | cut -d ' ' -f1) | gzip -6 > build-logs/`echo $LINE | cut -d ' ' -f2`.log.gz
+done < <(docker ps --filter "network=d3-${DOCKER_NETWORK}" --format "{{.ID}} {{.Names}}")
+
+tar -zcvf build-logs/notaryIrohaIntegrationTest.gz -C notary-iroha-integration-test/build/reports/tests integrationTest || true
+tar -zcvf build-logs/jacoco.gz -C build/reports jacoco || true
+tar -zcvf build-logs/dokka.gz -C build/reports dokka || true
